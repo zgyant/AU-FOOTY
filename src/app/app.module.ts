5847a91f9a5c1d59e06ea0314eb7278ca7858417
Jayant <jayant.mishra@recognite.com.au>
2019-04-25 15:37:55 +1000

    Changes to Route and Components
    Made new routes,
    changed to single page,
    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LadderComponent } from './ladder/ladder.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule} from '@angular/router';
+import { FixturesComponent } from './fixtures/fixtures.component';
+import { TipsComponent } from './tips/tips.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LadderComponent,
+    DashboardComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FixturesComponent,
+    TipsComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot([
+          {path: '', component: DashboardComponent},
+        {path: 'ladder', component: LadderComponent},
+          {path: 'fixtures', component: FixturesComponent},
+          {path: 'tips', component: TipsComponent},
+          {path: 'teams', component: TeamsComponent},
+          ])
   ],
   providers: [],
   bootstrap: [AppComponent]
